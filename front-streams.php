38fda68bb333a861dc6b874bce823198deced2a3
shiburizu <shiburizu312@gmail.com>
2023-04-14 08:02:12 -0600

    updates

--- front-streams.php
+++ front-streams.php
@@ -1,20 +1,20 @@
-<div class="panel sc-panel bg-dark sc-rounded">
+<div class="panel sc-panel bg-dark">
 <div class="panel-header">
     <h4 class="panel-title text-center mb-0 text-bold">FGC Streaming</h4>
 </div>
 <div class="panel-body px-0">
-    <div class="stream-box columns col-gapless">
-        <div class="column col-4 col-xl-12">
-            <img src="<?php echo get_template_directory_uri() . '/images/thumb.jpg' ?>" class="img-fit-contain img-responsive">
+    <div class="stream-box bg-gray mb-2 columns col-gapless">
+        <div class="column col-4 col-xl-12 p-2">
+            <img src="<?php echo get_template_directory_uri() . '/images/thumb.jpg' ?>" class="sc-rounded img-fit-contain img-responsive">
         </div>
         <div class="column col-8 p-2">
             <span class="text-tiny"><b>Streamer - Game</b></span><br>
             <span class="text-small">Stream Title</span>
         </div>
     </div>
-    <div class="stream-box columns col-gapless">
-        <div class="column col-4 col-xl-12">
-            <img src="<?php echo get_template_directory_uri() . '/images/thumb.jpg' ?>" class="img-fit-contain img-responsive">
+    <div class="stream-box bg-gray columns col-gapless">
+        <div class="column col-4 col-xl-12 p-2">
+            <img src="<?php echo get_template_directory_uri() . '/images/thumb.jpg' ?>" class="sc-rounded img-fit-contain img-responsive">
         </div>
         <div class="column col-8 p-2">
             <span class="text-tiny"><b>Povertycast - Game</b></span><br>
