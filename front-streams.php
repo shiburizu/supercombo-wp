f53437cb084cf98391b11f6716f0930939fc6f21
Kevin Higgins <shiburizu312@gmail.com>
2023-04-19 17:11:12 -0600

    make streams usable

--- front-streams.php
+++ front-streams.php
@@ -1,29 +1,25 @@
-<?php if ( is_user_logged_in() ) : ?>
 <div class="panel sc-panel bg-dark">
 <div class="panel-header">
-    <h4 class="panel-title text-center mb-0 text-bold">FGC Streaming</h4>
+    <h4 class="panel-title text-center mb-0 text-bold">Streaming Now</h4>
 </div>
 <div class="panel-body mb-2">
-    <div class="stream-box mb-2 columns col-gapless">
-        <div class="column col-4 col-xl-12">
-            <img src="<?php echo get_template_directory_uri() . '/images/thumb.jpg' ?>" class="sc-rounded img-fit-contain img-responsive">
-        </div>
-        <div class="column col-8 col-xl-12 p-2">
-            <span class="text-tiny"><b>Streamer - Game</b></span><br>
-            <span class="text-small">Stream Title</span>
-            <div class="divider py-0"></div>
-        </div>
+<?php
+$json = file_get_contents('https://supercombo.gg/wp-content/frontstreams.json');
+$json = json_decode($json);
+foreach($json as $key => $value): ?>
+<div class="stream-box columns col-gapless">
+    <div class="column col-6 col-xl-12 pb-2">
+        <a target="__blank" href="https://twitch.tv/<?php echo $key ?>">
+        <img src="<?php echo $value->thumb ?>" class="sc-rounded img-fit-contain img-responsive">
+        </a>
     </div>
-    <div class="stream-box mb-2 columns col-gapless">
-        <div class="column col-4 col-xl-12">
-            <img src="<?php echo get_template_directory_uri() . '/images/thumb.jpg' ?>" class="sc-rounded img-fit-contain img-responsive">
-        </div>
-        <div class="column col-8 col-xl-12 p-2">
-            <span class="text-tiny"><b>Povertycast - Game</b></span><br>
-            <span class="text-small">Stream Title</span>
-            <div class="divider py-0"></div>
-        </div>
+    <div class="column col-6 col-xl-12 px-2 pb-2">
+        <span class="text-small text-bold"><a target="__blank" href="https://twitch.tv/<?php echo $key ?>"><?php echo $key ?></a></span><br>
+        <span class="text-tiny text-bold"><?php echo $value->game ?></span><br>
     </div>
 </div>
+<span class="text-small text-tiny"><?php echo $value->title ?></span>
+<div class="divider pb-2"></div>
+<?php endforeach; ?>
 </div>
-<?php endif; ?>
+</div>
