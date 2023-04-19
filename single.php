7aebcc2c1fb927ffc3f26bcea5cfe13bde2bf460
Kevin Higgins <shiburizu312@gmail.com>
2023-04-19 12:49:31 -0600

    f

--- single.php
+++ single.php
@@ -8,7 +8,9 @@
             <div class="card-header px-0 mb-2">
                 <h1 class="h2 mb-2 pb-2 text-center"><a class="text-light text-bold" href="<?php the_permalink() ?>"><?php the_title() ?></a></h1>
                 <?php if ( wcount() >= 250 ) : ?>
+                    <?php if ( has_post_thumbnail() ) : ?>
                     <div class="sc-post-thumb sc-rounded" style="background-image: url('<?php echo get_the_post_thumbnail_url(get_the_ID(),'post-thumb') ?>')"></div>
+                    <?php endif; ?>
                     <?php if ( has_excerpt() ) : ?>
                     <p class="p-2 mt-2 mb-0 sc-featured-border text-light text-bold"><?php echo get_the_excerpt() ?></p>
                     <?php endif; ?>
@@ -51,7 +53,7 @@
                     </div>
                 <?php endif; ?>
             </div>
-            <div class="card-body <?php if (!has_post_thumbnail()) : echo "sc-rounded"; else: echo "px-0"; endif;?>">
+            <div class="card-body <?php if (wcount() < 250) : echo "sc-rounded"; else: echo "px-0"; endif;?>">
                 <?php the_content() ?>
                     <?php if ( !has_post_thumbnail() ): ?>
                         <div class="divider"></div>
