05e32d18e2f01ddf867b91f6470dca776208161f
Kevin Higgins <shiburizu312@gmail.com>
2023-04-19 18:45:27 -0600

    updates

--- front-streams.php
+++ front-streams.php
@@ -1,25 +1,25 @@
 <div class="panel sc-panel bg-dark">
-<div class="panel-header">
-    <h4 class="panel-title text-center mb-0 text-bold">Streaming Now</h4>
+<div class="panel-header pb-2 px-0">
+    <h4 class="panel-title text-center mb-2 text-bold">Streaming Now</h4>
 </div>
 <div class="panel-body mb-2 px-0">
 <?php
 $json = file_get_contents('https://supercombo.gg/wp-content/frontstreams.json');
 $json = json_decode($json);
 foreach($json as $key => $value): ?>
-<div class="stream-box columns col-gapless px-2">
-    <div class="column col-7 col-xl-12 pb-2">
+<div class="divider my-0"></div>
+<div class="stream-box columns col-gapless p-2">
+    <div class="column col-6 col-xl-12 p-2">
         <a target="__blank" href="https://twitch.tv/<?php echo $key ?>">
         <img loading="lazy" src="<?php echo $value->thumb ?>" class="sc-rounded img-fit-contain img-responsive">
         </a>
     </div>
-    <div class="column col-5 col-xl-12 px-2 pb-2">
-        <span class="text-small text-bold"><a target="__blank" href="https://twitch.tv/<?php echo $key ?>"><?php echo $key ?></a></span><br>
+    <div class="column col-6 col-xl-12 px-2">
+        <span class="text-tiny text-bold"><a target="__blank" href="https://twitch.tv/<?php echo $key ?>"><?php echo $key ?></a></span><br>
         <span class="text-tiny text-bold"><?php echo $value->game ?></span><br>
+        <p class="text-small text-tiny mb-2"><?php echo $value->title ?></p>
     </div>
 </div>
-<span class="text-small text-tiny"><?php echo $value->title ?></span>
-<div class="divider pb-2"></div>
 <?php endforeach; ?>
 </div>
 </div>
