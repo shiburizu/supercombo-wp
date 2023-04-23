24a62229a7473e4900928e77dd950afc4f552631
Kevin Higgins <shiburizu312@gmail.com>
2023-04-23 16:11:08 -0600

    f

--- page.php
+++ page.php
@@ -5,13 +5,12 @@
     <?php  if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
     <article class="sc-article p-centered">
         <div class="card bg-dark sc-article-body p-centered">
-            <div class="card-header px-0 mb-2">
+            <div class="card-header px-0 pb-2 mb-2">
                 <h1 class="h2 mb-2 pb-2 text-center"><a class="text-light text-bold" href="<?php the_permalink() ?>"><?php the_title() ?></a></h1>
                 <?php if ( wcount() >= 150 ) : ?>
                     <?php if ( has_post_thumbnail() ) : ?>
                     <div class="sc-post-thumb sc-rounded" style="background-image: url('<?php echo get_the_post_thumbnail_url(get_the_ID(),'post-thumb') ?>')"></div>
                     <?php endif; ?>
-                    <div class="divider pb-2"></div>
                 <?php endif; ?>
             </div>
             <div class="card-body <?php if (wcount() < 150) : echo "sc-rounded"; else: echo "p-0"; endif;?>">
