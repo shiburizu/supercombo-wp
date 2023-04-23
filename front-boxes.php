2ec94c5c3a506e76496b35946c6ef8452367b0ad
Kevin Higgins <shiburizu312@gmail.com>
2023-04-22 18:10:06 -0600

    f

--- front-boxes.php
+++ front-boxes.php
@@ -31,12 +31,12 @@ if (count($obj->data) > 0) {
                             <p class="text-small text-gray d-inline mb-1">
                                 <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ) ?>" class="text-bold">
                                     <?php the_author() ?>
-                                </a> - <?php echo $post_time ?><?php if ($comments != null) :?> - <a href="<?php the_permalink() ?>"><?php if ($comments > 1) : echo $comments . " comments"; elseif ($comments == 1): echo $comments . " comment"; else: echo "Discuss"; endif;?> <?php endif;?></p>
+                                </a> - <?php echo $post_time ?><?php if (!empty($comments)) :?> - <a href="<?php the_permalink() ?>"><?php if ($comments > 1) : echo $comments . " comments"; elseif ($comments == 1): echo $comments . " comment"; else: echo "Discuss"; endif;?> <?php endif;?></p>
                         <?php else: ?>
                             <p class="text-small text-gray d-inline mb-1">
                                 <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ) ?>" class="text-bold">
                                     <?php the_author() ?>
-                                </a> - <?php echo get_the_date() ?><?php if ($comments != null) :?> - <a href="<?php the_permalink() ?>"><?php if ($comments > 1) : echo $comments . " comments"; elseif ($comments == 1): echo $comments . " comment"; else: echo "Discuss"; endif;?> <?php endif;?></p>
+                                </a> - <?php echo get_the_date() ?><?php if (!empty($comments)) :?> - <a href="<?php the_permalink() ?>"><?php if ($comments > 1) : echo $comments . " comments"; elseif ($comments == 1): echo $comments . " comment"; else: echo "Discuss"; endif;?> <?php endif;?></p>
                         <?php endif; ?>
                     </span>
                     <h2 class="h4 my-2"><a class="text-light text-bold" href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
@@ -61,9 +61,9 @@ if (count($obj->data) > 0) {
                         $post_time = "Just now";
                     }
                 ?>
-                <p class="text-small text-gray d-inline mb-1"><a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ) ?>" class="text-bold"><?php the_author() ?></a> - <?php echo $post_time ?><?php if ($comments != null) :?> - <a href="<?php the_permalink() ?>"><?php if ($comments > 1) : echo $comments . " comments"; elseif ($comments == 1): echo $comments . " comment"; else: echo "Discuss"; endif;?><?php endif;?></p>
+                <p class="text-small text-gray d-inline mb-1"><a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ) ?>" class="text-bold"><?php the_author() ?></a> - <?php echo $post_time ?><?php if (!empty($comments)) :?> - <a href="<?php the_permalink() ?>"><?php if ($comments > 1) : echo $comments . " comments"; elseif ($comments == 1) : echo $comments . " comment"; else: echo "Discuss"; endif;?><?php endif;?></p>
             <?php else: ?>
-                <p class="text-small text-gray d-inline mb-1"><a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ) ?>" class="text-bold"><?php the_author() ?></a> - <?php echo get_the_date() ?><?php if ($comments != null) :?> - <a href="<?php the_permalink() ?>"><?php if ($comments > 1) : echo $comments . " comments"; elseif ($comments == 1): echo $comments . " comment"; else: echo "Discuss"; endif;?><?php endif;?></p>
+                <p class="text-small text-gray d-inline mb-1"><a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ) ?>" class="text-bold"><?php the_author() ?></a> - <?php echo get_the_date() ?><?php if (!empty($comments)) :?> - <a href="<?php the_permalink() ?>"><?php if ($comments > 1) : echo $comments . " comments"; elseif ($comments == 1) : echo $comments . " comment"; else: echo "Discuss"; endif;?><?php endif;?></p>
             <?php endif; ?>
         </span>
         <?php if ( wcount() > 150 ) : ?>
