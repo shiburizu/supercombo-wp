8431481b62e848484139fad6d9e314e3af9db8bc
Kevin Higgins <shiburizu312@gmail.com>
2023-04-22 18:01:35 -0600

    f

--- front-boxes.php
+++ front-boxes.php
@@ -6,11 +6,11 @@ $post_age = date('U') - get_post_time('U');
 $comments = null;
 $json = file_get_contents( 'https://forums.supercombo.gg/api/discussions?filter%5Bq%5D=wordpressid:' . strval(get_the_ID()) );
 $obj = json_decode($json);
-if (count($obj->data) != 0) {
+if (count($obj->data) > 0) {
     $comments = $obj->data[0]->attributes->commentCount;
 }
 ?>
-<div class="d-none"><?php echo count($obj->data) ?></div>
+<div class="d-none"><?php echo count($comments) ?></div>
 <?php if ( wcount() > 150 and has_post_thumbnail() ) : ?>
     <?php if ( is_front_page() and $first == False and !is_paged() ) :?>
         <?php $first = True; ?>
