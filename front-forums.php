cf18b68044589ac6a4cc4516e91958b7b8d5f338
Kevin Higgins <shiburizu312@gmail.com>
2023-04-26 17:50:14 -0600

    f

--- front-forums.php
+++ front-forums.php
@@ -1,14 +1,9 @@
 <?php $forum_feed = simplexml_load_file("https://forums.supercombo.gg/atom");
 for ( $i = 0; $i < 10; $i++ ) : ?>
 <div class="tile">
-    <?php $post_time = esc_html( human_time_diff( strtotime(strval($forum_feed->entry[$i]->updated)), current_time('timestamp') ) ) . ' ago';
-        if ( str_contains($post_time, 'min') or str_contains($post_time, 'second') ) {
-            $post_time = "just now";
-        }
-    ?>
     <div class="tile-content mx-2">
         <a target="__blank" href="<?php echo $forum_feed->entry[$i]->id; ?>"class="tile-title mb-0"><?php echo $forum_feed->entry[$i]->title; ?></a>
-        <p class="tile-subtitle text-gray mb-2">By <?php echo $forum_feed->entry[$i]->author->name; ?> <?php echo $post_time ?></p>
+        <p class="tile-subtitle text-gray mb-2">By <?php echo $forum_feed->entry[$i]->author->name; ?></p>
         <p class="mb-1 text-tiny" style="word-break: break-word;"><?php echo atom_excerpt(strval($forum_feed->entry[$i]->content)) ?></p>
     </div>
 </div>
--- front-wiki.php
+++ front-wiki.php
@@ -1,15 +1,9 @@
 <?php $wiki_feed = simplexml_load_file("https://wiki.supercombo.gg/api.php?hidebots=1&urlversion=1&days=7&limit=20&action=feedrecentchanges&feedformat=atom");
 for ( $w = 0; $w < 10; $w++ ) : ?>
 <div class="tile">
-    <?php $post_time = esc_html( human_time_diff( strtotime(strval($wiki_feed->entry[$w]->updated)), current_time('timestamp') ) ) . ' ago';
-        if ( str_contains($post_time, 'min') or str_contains($post_time, 'second') ) {
-            $post_time = "just now";
-        }
-    ?>
     <div class="tile-content mx-2">
         <a target="__blank" href="<?php echo $wiki_feed->entry[$w]->id; ?>"class="tile-title mb-0"><?php echo $wiki_feed->entry[$w]->title; ?></a>
-        <p class="tile-subtitle text-gray mb-2">By <?php echo $wiki_feed->entry[$w]->author->name; ?> <?php echo 
-        $post_time ?></p>
+        <p class="tile-subtitle text-gray mb-2">By <?php echo $wiki_feed->entry[$w]->author->name; ?></p>
         <a target="__blank" href="<?php echo $wiki_feed->entry[$w]->id; ?>" class="mb-1 text-tiny text-light" style="word-break: break-word;"><?php echo atom_excerpt(strval($wiki_feed->entry[$w]->summary)) ?></a>
     </div>
 </div>
