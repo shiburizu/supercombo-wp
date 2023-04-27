a64bace7d4955f1fc1e13755faedef04ec3cbed5
shiburizu <shiburizu312@gmail.com>
2023-04-26 19:52:37 -0600

    f

--- front-ambox.php
+++ front-ambox.php
@@ -1,13 +1,13 @@
 <?php if ( "2023-07-14" > date('Y-m-d')) : ?>
 <div class="d-flex py-2" style="justify-content: center;">
-<div class="featured-ambox mx-0 columns sc-rounded p-2">
+<div class="featured-ambox mx-0 columns sc-rounded">
 <div class="p-2 col-10 col-lg-12 d-flex" style="flex-direction: column; justify-content: center;"> 
-    <h5 class="text-bold mb-0"><a class="text-light" href="https://www.start.gg/tournament/defend-the-north-2023-powered-by-msi/details">Don't miss Defend The North, the largest PC FGC Tournament. July 14-16 in Brooklyn, NY!</a></h5>
+    <h6 class="text-bold mb-0"><a class="text-light" href="https://www.start.gg/tournament/defend-the-north-2023-powered-by-msi/details">Don't miss Defend The North, the largest PC FGC Tournament. July 14-16 in Brooklyn, NY!</a></h6>
     <span class="mt-1">
         <p class="d-inline mb-1"><a class="text-bold text-light text-tiny" href="https://www.start.gg/tournament/defend-the-north-2023-powered-by-msi/details"> Use code "SUPERCOMBO" at registration to get $5 off!</a></p>
     </span>
 </div>
-<a href="https://www.start.gg/tournament/defend-the-north-2023-powered-by-msi/details" class="column col-2 hide-lg px-2 sc-thumb-top" style="background: url('<?php echo get_template_directory_uri(); ?>/images/ambox/dtn2023.png')">   
+<a href="https://www.start.gg/tournament/defend-the-north-2023-powered-by-msi/details" class="column col-2 hide-lg px-2 sc-thumb-bg" style="background: url('<?php echo get_template_directory_uri(); ?>/images/ambox/dtn2023.png')">   
 </a>
 </div>
 </div>
