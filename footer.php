8385c3ddeec583ffdc25d2f69202615a075cb45e
Kevin Higgins <shiburizu312@gmail.com>
2023-04-27 16:27:20 -0600

    f

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
         <div class="column col-6 col-md-12 text-center d-flex p-2" style="flex-direction: column; justify-content: center;">
             <img class="img-responsive p-2" src="<?php echo esc_url( wp_get_attachment_url( get_theme_mod( 'custom_logo' ) ) ); ?>">
             <div class="text-gray">
-                <p class="my-2">The home of competitive fighting games. The home of competitive fighting games. Breaking News, Tournament Coverage, Community Wikis, Forums and much more for the FGC.</p>
+                <p class="my-2">The home of competitive fighting games. Breaking News, Tournament Coverage, Community Wikis, Forums and much more for the FGC.</p>
                 <p class="my-2 text-bold">Est. X.XXX.MMXXI</p>
                 <a href="https://shib.live">Made with 💙 by @shiburizu</a> - <a style="color: #60A000;" href="https://gbl.gg">Powered by GBL</a>
             </div>
