50d0c1d90282fb76b9980289d7f43eeb11efec19
Kevin Higgins <shiburizu312@gmail.com>
2023-04-26 18:57:50 -0600

    f

--- front-ambox.php
+++ front-ambox.php
@@ -2,7 +2,6 @@
 <div class="d-flex py-2" style="justify-content: center;">
 <div class="featured-ambox mx-0 mb-2 columns sc-featured-border">
 <div class="p-2 col-10 col-lg-12 d-flex" style="flex-direction: column; justify-content: center;"> 
-    <span class="text-gray text-bold text-small">#Sponsored</span>
     <h5 class="text-bold mb-0"><a class="text-light" href="https://www.start.gg/tournament/defend-the-north-2023-powered-by-msi/details">Don't miss Defend The North, the largest PC FGC Tournament. July 14-16 in Brooklyn, NY!</a></h5>
     <span class="mt-1">
         <p class="d-inline mb-1"><a class="text-bold text-light text-tiny" href="https://www.start.gg/tournament/defend-the-north-2023-powered-by-msi/details"> Use code "SUPERCOMBO" at registration to get $5 off!</a></p>
