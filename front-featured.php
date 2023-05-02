77063154f2af5a6e3fe9db5c59f901e4740adae9
shiburizu <shiburizu312@gmail.com>
2023-05-01 23:58:55 -0600

    f

--- front-featured.php
+++ front-featured.php
@@ -44,7 +44,7 @@
                 <div class="columns sc-featured-border">
                 <?php if (has_post_thumbnail($featured[$i])) : ?>
                 <div class="p-2 col-9 d-flex" style="flex-direction: column; justify-content: center;"> 
-                    <h5 class="text-bold mb-0"><a class="text-light" href="<?php the_permalink($featured[$i]) ?>"><?php echo $featured[$i]->post_title; ?></a></h5>
+                    <h6 class="text-bold mb-0"><a class="text-light" href="<?php the_permalink($featured[$i]) ?>"><?php echo $featured[$i]->post_title; ?></a></h6>
                     <span class="mt-1">
                             <p class="text-tiny text-gray d-inline mb-1"><a href="<?php echo get_author_posts_url( $featured[$i]->post_author ) ?>" class="text-bold"><?php echo the_author_meta( 'display_name' , $featured[$i]->post_author ); ?></a> - <?php echo get_the_date('',$featured[$i]) ?><?php echo $comment_link ?></p>
                     </span>
@@ -54,7 +54,7 @@
                 </div>
                 <?php else: ?>
                 <div class="p-2 col-12 d-flex" style="flex-direction: column; justify-content: center;"> 
-                    <h5 class="text-bold mb-0"><a class="text-light" href="<?php the_permalink($featured[$i]) ?>"><?php echo $featured[$i]->post_title; ?></a></h5>
+                    <h6 class="text-bold mb-0"><a class="text-light" href="<?php the_permalink($featured[$i]) ?>"><?php echo $featured[$i]->post_title; ?></a></h6>
                     <span class="mt-1">
                         <p class="text-gray d-inline mb-1"><a class="text-bold text-tiny" href="<?php echo get_author_posts_url( $featured[$i]->post_author ) ?>"><?php echo the_author_meta( 'display_name' , $featured[$i]->post_author ); ?></a><span class="text-tiny"> - <?php echo get_the_date('',$featured[$i]) ?><?php echo $comment_link ?></span></p>
                     </span>
