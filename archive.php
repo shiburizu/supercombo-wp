d8da1e085ff4ea0236a28c2fb95329c90293b7c2
shiburizu <shiburizu312@gmail.com>
2024-09-05 03:33:42 -0400

    m

--- archive.php
+++ archive.php
@@ -5,14 +5,12 @@
     <div class="columns sc-archive">
         <div class="column col-lg-12 p-centered">
             <div class="text-light">
-            <div class="divider pb-2"></div>
             <?php if ( !is_front_page() ) : ?>
             <h1 class="h2 text-bold"><?php echo get_the_archive_title() ?></h1>
             <p><?php the_archive_description() ?></p>
             <?php else: ?>
             <h1 class="h2 pt-2 text-bold">All Posts</h2>
             <?php endif; ?>
-            <div class="divider"></div>
             <?php echo bootstrap_pagination(); ?>
             <?php get_template_part( 'front-boxes' ); ?>
             <?php echo bootstrap_pagination() ?>
