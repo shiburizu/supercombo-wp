30900f4fe448b0477695db6177b779f76a86688a
shiburizu <shiburizu312@gmail.com>
2024-09-08 13:37:19 -0400

    update radio

--- front-radio-player.php
+++ front-radio-player.php
@@ -2,10 +2,8 @@
 <div class="panel-header pb-2 px-0" style="border-bottom: 1px solid #2c2c2c;">
     <h4 class="panel-title text-center mb-2 text-bold">FGC Radio Player</h4>
 </div>
-<div class="panel-body mb-2 px-0 masked-overflow">
-<div class="px-1 pt-2">
-<iframe src="https://radio.fgcast.com/public/fgc_radio/embed" frameborder="0" allowtransparency="true" style="width: 100%; height: 150px; border: 0;"></iframe>
-</div>
+<div class="panel-body p-0">
+<iframe src="https://tunein.com/embed/player/s331675/?background=dark" style="width:100%; height:100px;" scrolling="no" frameborder="no"></iframe>
 </div>
 <div class="panel-footer m-0 py-1 px-0" style="border-top: 1px solid #2c2c2c;">
     <div class="px-1 py-0 btn-group btn-group-block text-bold">
