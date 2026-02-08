c862a2062642d702c50960eacbf0413cb31a5011
shiburizu <shiburizu312@gmail.com>
2026-02-08 02:55:13 -0500

    padding m

--- author.php
+++ author.php
@@ -31,11 +31,11 @@
         <div class="column col-4 col-lg-12">
             <?php get_template_part( 'sidebar-top' ); ?>
             <?php get_template_part( 'front-streams' ); ?>
-            <div style="margin: 0.2rem 0rem;"><div id="sidebar-banner-1"></div></div>
+            <div style="margin: 0.8rem 0rem;"><div id="sidebar-banner-1"></div></div>
             <?php get_template_part( 'front-community' ); ?>
-            <div style="margin: 0.2rem 0rem;"><div id="sidebar-banner-2"></div></div>
+            <div style="margin: 0.8rem 0rem;"><div id="sidebar-banner-2"></div></div>
             <?php get_template_part( 'front-youtube-sidebar' ); ?>
-            <div style="margin: 0.2rem 0rem;"><div id="sidebar-banner-3"></div></div>
+            <div style="margin: 0.8rem 0rem;"><div id="sidebar-banner-3"></div></div>
         </div>
     </div>
     <div style="padding-top: 0.4rem; max-height: 270px;">
